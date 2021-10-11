0f709a0a772bb7fa4017c7158e4a5a6ba79f0b31
matteo-bartoli <bartolimatteo.03@gmail.com>
2021-10-11 13:31:36 +0200

    esercizi informatica

---
+++ Cimara_2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <title>Indovina Numero</title>
+</head>
+<body>
+    <form action="/action_page.php">
+    <label for="quantity">Quantity:</label>
+    <input type="number" id="quantity" name="quantity" min="0" max="100">
+    <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
