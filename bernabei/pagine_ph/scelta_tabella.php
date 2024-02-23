f071d6226659344a7e0987d79908caf81865c053
iacopo <iacopolibero@gmail.com>
2024-02-23 10:29:33 +0100

    iniziato esercizio 3

---
+++ bernabei/pagine_ph/scelta_tabella.php
@@ -0,0 +1,19 @@
+<?php
+include('connessione.php');  // Questo richiama la connessione quindi possiamo usare $conn in questa pagina
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    // Ottengo il valore della form
+   
+    <a href="index.html">Torna alla home page</a>
+</body>
+</html>
